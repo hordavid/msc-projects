b6ab8b7109edd72cf1d7442cf236e56ffe00d9fc
hordavid <horvathdavid@upcmail.hu>
2019-02-25 02:28:53 +0100

    ORSI bead1 v1.

--- orsi/bead1/impl.cpp
+++ orsi/bead1/impl.cpp
@@ -28,7 +28,7 @@ int field_value(const FIELD& f)
 
 int tile_value(const Tile& t)
 {
-    return field_value(std::get<1>(t));
+    return field_value(t.second);
 }
 
 bool operator<(const Coordinate& a, const Coordinate& b)
@@ -46,12 +46,10 @@ std::ostream& operator<<(std::ostream& s, const FIELD& f)
 {
     return s;
 }
-
 std::ostream& operator<<(std::ostream& s, const DIRECTION& d)
 {
     return s;
 }
-
 std::ostream& operator<<(std::ostream& s, const Coordinate& p)
 {
     return s;
@@ -60,7 +58,6 @@ std::istream& operator>>(std::istream& s, Coordinate& p)
 {
     return s;
 }
-
 std::ostream& operator<<(std::ostream& s, const Tile& t)
 {
     return s;
@@ -70,10 +67,9 @@ std::istream& operator>>(std::istream& s, Map& m)
 {
     s >> m.rows_ >> m.cols_;
 
-    m.map_.resize(m.rows());
+    m.map_.resize(m.rows(), std::vector<FIELD>(m.cols()));
     for (int r = 0; r < m.rows(); r++)
     {
-        m.map_[r].resize(m.cols());
         for (int c = 0; c < m.cols(); c++)
         {
             int f_ind;
@@ -134,11 +130,47 @@ void Map::set_tile(const int i, const int j, const FIELD f)
 
 Tile Map::tile_in_direction(int x, int y, const DIRECTION d) const
 {
-    return tile_at(0,0);
+    const bool y_is_even = y % 2 == 0;
+
+    switch (d)
+    {
+        case DIRECTION::NORTH:
+            return tile_at(x - 1, y);
+        case DIRECTION::SOUTH:
+            return tile_at(x + 1, y);
+        case DIRECTION::NORTH_EAST:
+            return tile_at(y_is_even ? x - 1 : x, y + 1);
+        case DIRECTION::NORTH_WEST:
+            return tile_at(y_is_even ? x - 1 : x, y - 1);
+        case DIRECTION::SOUTH_EAST:
+            return tile_at(y_is_even ? x : x + 1, y + 1);
+        case DIRECTION::SOUTH_WEST:
+            return tile_at(y_is_even ? x : x + 1, y - 1);
+        default:
+            return tile_at(x, y);
+    }
 }
 
 std::set<Tile> Map::get_tiles_in_radius(const int i, const int j, const int r) const
 {
     std::set<Tile> s;
+
+    s.insert(tile_at(i, j));
+
+    if (0 < r)
+    {
+        const DIRECTION ds[] = {DIRECTION::NORTH, DIRECTION::SOUTH,
+            DIRECTION::NORTH_EAST, DIRECTION::NORTH_WEST,
+            DIRECTION::SOUTH_EAST, DIRECTION::SOUTH_WEST};
+
+        for (const auto &d: ds)
+        {
+            const Coordinate c = std::get<0>(tile_in_direction(i, j, d));
+
+            const std::set<Tile> ts = get_tiles_in_radius(c.x, c.y, r - 1);
+            s.insert(ts.begin(), ts.end()); 
+        }
+    }
+
     return s;
 }
--- orsi/bead1/main.cpp
+++ orsi/bead1/main.cpp
@@ -1,30 +1,73 @@
 #include "types.hpp"
-#include <iostream>
 #include <fstream>
+#include <numeric>
+#include <algorithm>
+#include <thread>
+#include <future>
+
+
+std::vector<Coordinate> get_coordinates(std::ifstream &in, const int c);
+void psum(const Map &m, const Coordinate &c, const int r, std::promise<int> *p);
+
 
 int main(int argc, char** argv)
 {
     int c;
     int r;
     Map m;
+
+
     std::ifstream in("world.map");
 
     in >> m >> c >> r;
 
-    // @TODO: szálakat implementálni
+    std::vector<Coordinate> cs = get_coordinates(in, c);
 
+    in.clear();
     in.close();
     
-    std::ofstream out ("output.txt");
-    if (out.is_open())
+
+    std::ofstream out("output.txt");
+    std::vector<std::thread> ts;
+
+    for (const auto &c : cs)
     {
-        out << "rows=" << m.rows() << std::endl;
-        out << "cols=" << m.cols() << std::endl;
-        out << "c=" << c << std::endl;
-        out << "r=" << r << std::endl;
-        out.close();
+        std::promise<int> p;
+        std::future<int> f = p.get_future();
+        ts.push_back(std::thread(psum, m, c, r, &p));
+
+        out << f.get() << std::endl;
     }
-    else std::cout << "Unable to open file";
+
+    std::for_each(ts.begin(), ts.end(), [](std::thread &t) { t.join(); });
+
+    out.clear();
+    out.close();
 
     return 0;
+}
+
+
+std::vector<Coordinate> get_coordinates(std::ifstream &in, const int c)
+{
+    std::vector<Coordinate> cs;
+    
+    for (int i = 0; i < c; i++)
+    {
+        Coordinate coord;
+        in >> coord.x >> coord.y;
+        --coord.x;
+        --coord.y;
+
+        cs.push_back(coord);
+    }
+
+    return cs;
+}
+
+void psum(const Map &m, const Coordinate &c, const int r, std::promise<int> *p) 
+{
+    std::set<Tile> s = m.get_tiles_in_radius(c.x, c.y, r);
+
+    p->set_value(std::accumulate(s.begin(), s.end(), 0, [](int x, const Tile &t) { return x + field_value(t.second); }));
 }
